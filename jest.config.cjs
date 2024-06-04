65c82fa48332adc22ce1cd37748738f031bb2329
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-06-04 22:55:12 +0500

    jest tests working but changed to cjs files error

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+// jest.config.js
+
+module.exports = {
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.js$": "babel-jest", // If using Babel
+  },
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"], // Add this line to specify where your source files are
+};
---
+++ src/components/myModule.js
@@ -0,0 +1,5 @@
+function MyComponent(a, b) {
+  return a + b;
+}
+
+export default MyComponent;
